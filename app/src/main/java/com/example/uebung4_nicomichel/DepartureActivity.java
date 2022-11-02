6a0f83e6958833218dee57a7c32712001be099a6
Nico Michel <nico@mail-michel.com>
2022-11-02 14:36:20 +0100

    Initial commit

---
+++ app/src/main/java/com/example/uebung4_nicomichel/DepartureActivity.java
@@ -0,0 +1,14 @@
+package com.example.uebung4_nicomichel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DepartureActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_departure);
+    }
+}
---
+++ app/src/main/java/com/example/uebung4_nicomichel/view.java
@@ -0,0 +1,4 @@
+package com.example.uebung4_nicomichel;
+
+public class view {
+}
